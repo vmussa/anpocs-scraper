76db533bebe3b279cd113daa6f711aead69274f8
Vitor Mussa <vtrmussa@gmail.com>
2021-04-12 18:03:34 -0300

    Adiciona parte da raspagem dos resumos

--- src/anpocs43.py
+++ src/anpocs43.py
@@ -23,27 +23,64 @@ def is_title_or_session(tag):
     is_child_of_span = tag.parent.name == 'span'
     is_strong = tag.name == 'strong'
     excluded = tag.text == "Palavras-chave:"
-    return is_child_of_span and is_strong and not excluded
+    insider = is_inside_div(tag)
+    return (
+        is_child_of_span and is_strong and not excluded and insider
+    )
+
+def is_title(tags):
+    titles = tags[1::2]
+    return titles
+
+def is_session(tags):
+    sessions = tags[::2]
+    return sessions
+
+def is_author(tag):
+    pass
+
+def is_abstract(tag):
+    pass
+
+def get_titles_and_session_tags():
+    tags = soup.find_all(is_title_or_session)
+    return tags
 
 def get_tags_texts(tags):
     texts = [tag.text for tag in tags]
     return texts
 
-def get_titles(tags):
-    titles = tags[1::2]
+def get_titles():
+    tags = get_titles_and_session_tags()
+    titles_tags = is_title(tags)
+    titles = [title.text for title in titles_tags]
     return titles
 
-def get_sessions(tags):
-    sessions = tags[::2]
+def get_sessions():
+    tags = get_titles_and_session_tags()
+    sessions_tags = is_session(tags)
+    sessions = [session.text for session in sessions_tags]
     return sessions
 
-def main():
-    tags_found = soup.find_all(is_title_or_session)
-    tags = [tag for tag in tags_found if is_inside_div(tag)]
-    tags_texts = get_tags_texts(tags)
+def get_authors():
+    authors_tags = soup.find_all(is_author)
+    authors = [author.text for author in authors_tags]
+    return authors
 
-    titles = get_titles(tags_texts)
-    sessions = get_sessions(tags_texts)
+def get_abstracts_tags():
+    tags = soup.find_all(is_abstract)
+    return tags
+
+def get_abstracts(tags):
+    # abstracts = is_abstract(tags)
+    # return abstracts
+    pass
+
+def main():
+    titles = get_titles()
+    sessions = get_sessions()
+    #abstracts = get_abstracts()
+    #authors = get_authors()
 
     data = {
         'autores': None,
@@ -55,6 +92,7 @@ def main():
 
     df = pd.DataFrame(data)
     df.to_csv('resumos_anpocs43.csv', index=False)
+    import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
     main()
