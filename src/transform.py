513eaa63ca4d9a02012d258821655db39466f465
Vitor Mussa <vtrmussa@gmail.com>
2021-04-20 19:19:57 -0300

    Inicia etapa de transformação da pipeline

---
+++ src/transform.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def clean_anpocs43_data():
+    pass
+
+def clean_anpocs44_data():
+    pass
+
+def main():
+    clean_anpocs43_data()
+    clean_anpocs44_data()
+
+if __name__ == "__main__":
+    main()
