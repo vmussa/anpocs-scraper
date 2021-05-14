d3faadc700f753367cb45b9cd5aaa1d4abd82dec
Daniel Mendes <daniel_mnds34@hotmail.com>
2021-05-14 20:59:22 -0300

    Adiciona transformação no anpocs43

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 def clean_anpocs43_data():
-    pass
+    df = pd.read_csv('../anpocs-scraper/output/resumos_anpocs43.csv')
+    df['autores'].replace(r' ST\d\d| SPG\d\d', value='', inplace=True, regex=True)
+    df['autores'] = df['autores'].str.strip()
+    df['titulo'] = df['titulo'].str.strip()
+    df['resumo'] = df['resumo'].str.strip()
+    df['sessao'] = df['sessao'].str.strip()
+    return df
 
 def clean_anpocs44_data():
     pass
