f1b7e9aff9e97ae96f308f1c076c15f1e8609fa6
Vitor Mussa <vtrmussa@gmail.com>
2021-05-14 21:37:49 -0300

    Completa pipeline de transformação dos dados

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-import anpocs43, anpocs44
+import anpocs43, anpocs44, transform
 
 if __name__ == "__main__":
     anpocs43.main()
     anpocs44.main()
+    transform.main()
--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTPUT_PATH = f"{BASE_PATH}{os.sep}output{os.sep}"
+
 
 def clean_anpocs43_data():
-    df = pd.read_csv('../anpocs-scraper/output/resumos_anpocs43.csv')
+    df = pd.read_csv(f'{OUTPUT_PATH}resumos_anpocs43.csv')
     df['autores'].replace(r' ST\d\d| SPG\d\d', value='', inplace=True, regex=True)
     df['autores'] = df['autores'].str.strip()
     df['titulo'] = df['titulo'].str.strip()
@@ -9,20 +14,28 @@ def clean_anpocs43_data():
     df['sessao'] = df['sessao'].str.strip()
     return df
 
+
 def clean_anpocs44_data():
-    df2 = pd.read_csv('../output/resumos_anpocs44.csv')
-    df2['autores'] = df2['autores'].str.strip()
-    df2['titulo'] = df2['titulo'].str.strip()
-    df2['resumo'] = (df2['resumo']
-                 .replace('^Resumo:', '', regex=True)
-                 .replace('Ocultar$', '', regex=True)
-                 .str.strip())
-    df2['sessao'] = (df2['sessao']
-                 .replace(r'(?<=GT\d\d).+|(?<=SPG\d\d).+', '', regex=True)
-                 .str.strip())
+    df = pd.read_csv(f'{OUTPUT_PATH}resumos_anpocs44.csv')
+    df['autores'] = df['autores'].str.strip()
+    df['titulo'] = df['titulo'].str.strip()
+    df['resumo'] = (df['resumo']
+                    .replace('^Resumo:', '', regex=True)
+                    .replace('Ocultar$', '', regex=True)
+                    .str.strip()
+                    .apply(lambda x: " ".join(x.split('\n'))))
+    df['sessao'] = (df['sessao']
+                    .replace(r'(?<=GT\d\d).+|(?<=SPG\d\d).+', '', regex=True)
+                    .str.strip())
+    return df
+
 
 def main():
-    pass
+    anpocs43 = clean_anpocs43_data()
+    anpocs44 = clean_anpocs44_data()
+    df = pd.concat([anpocs43, anpocs44]).reset_index(drop=True)
+    df.to_csv(f'{OUTPUT_PATH}resumos_anpocs.csv')
+
 
 if __name__ == "__main__":
     main()
