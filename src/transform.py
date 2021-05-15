2f62bac839577c59a114c6c5c2178ca29b1d6e19
Vitor Mussa <vtrmussa@gmail.com>
2021-05-14 21:04:45 -0300

    Adiciona transformação de anpocs44

--- src/transform.py
+++ src/transform.py
@@ -4,11 +4,19 @@ def clean_anpocs43_data():
     pass
 
 def clean_anpocs44_data():
-    pass
+    df2 = pd.read_csv('../output/resumos_anpocs44.csv')
+    df2['autores'] = df2['autores'].str.strip()
+    df2['titulo'] = df2['titulo'].str.strip()
+    df2['resumo'] = (df2['resumo']
+                 .replace('^Resumo:', '', regex=True)
+                 .replace('Ocultar$', '', regex=True)
+                 .str.strip())
+    df2['sessao'] = (df2['sessao']
+                 .replace(r'(?<=GT\d\d).+|(?<=SPG\d\d).+', '', regex=True)
+                 .str.strip())
 
 def main():
-    clean_anpocs43_data()
-    clean_anpocs44_data()
+    pass
 
 if __name__ == "__main__":
     main()
